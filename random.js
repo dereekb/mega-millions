749bdfb829b8595b6e183f56c661d97cbe94ad6b
Derek Burgman <dereekb@gmail.com>
2018-10-20 17:03:21 -0500

    Named Constants

--- random.js
+++ random.js
@@ -1,5 +1,18 @@
 const POWER_BALL_NUMBERS = 70;
 
+// isLucky Generator
+const K_BASE = 3;
+const K_MULTI = 17;
+const L_SPREAD = 100000;
+
+// Lucky Play
+const L_PLAY = 200;
+const L_MATCH = 20;
+
+if (L_PLAY < L_MATCH) {
+  throw new Error('No plays will be lucky!');
+}
+
 let numbers = []; // From 1 to 70
 for (let i = 1; i <= POWER_BALL_NUMBERS; i += 1) {
   numbers.push(i);
@@ -9,8 +22,8 @@ function isFeelingLuckyGenerator(x) {
   let k;
 
   return (x) => {
-    k = Math.floor(Math.random() * 15) + 4;
-    return (Math.floor(Math.random() * 10000) % k) === 0;
+    k = Math.floor(Math.random() * K_MULTI) + K_BASE;
+    return (Math.floor(Math.random() * L_SPREAD) % k) === 0;
   }
 }
 
@@ -44,7 +57,7 @@ function pickNumbers(count = 5) {
 }
 
 function isLuckyPlay(picks, megaBall) {
-  return Math.ceil(Math.random() * 100) === 100;
+  return Math.ceil(Math.random() * L_PLAY) === L_MATCH;
 }
 
 function generateMegaMillionsPlay() {
